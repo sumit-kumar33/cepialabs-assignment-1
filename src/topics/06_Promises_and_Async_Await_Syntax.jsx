4e13037f0812ce3e28fd718711ddff0505b2bae2
Sumit Kumar <s0k00singh@gmail.com>
2025-11-19 05:12:12 +0530

    finished 05

--- src/topics/06_Promises_and_Async_Await_Syntax.jsx
+++ src/topics/06_Promises_and_Async_Await_Syntax.jsx
@@ -1,16 +1,43 @@
-function PromisesAndAsyncAwaitSyntax() {
-  // Your JavaScript code for this topic will go here.
-  // For now, let's just return some HTML-like code.
+import React from "react";
 
-  const myName = "Learner";
-  const topic = "JavaScript Basics";
+function PromisesAndAsyncAwaitSyntax() {
+  // Promises are used to resolve asynchronous code to make it resolve like normal, synchronous code, which we can read from top to bottom.
+  // It traditionally use callbacks to resolve our asynchronous code.
+  // We use the .then() callback to resolve successfully resolved promises,
+  // while we use the .catch() callback to resolve promises that respond with an error.
+  // But that can make our code harder to read and maintain. And lead to "callback hell".
+  // Async/Await syntax provides a cleaner and more readable way to work with promises.
+  // With Async/Await, we can write asynchronous code that looks and behaves like synchronous code.
+  // This makes it easier to understand and maintain our code.
+
+  const topic = "Promises + Async/Await Syntax";
+  const [avatar, setAvatar] = React.useState("");
+  React.useEffect(() => {
+    async function fetchAvatar() {
+      const response = await fetch("https://api.github.com/users/sumit-kumar33");
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setAvatar(data.avatar_url);
+    }
+    fetchAvatar();
+  }, []);
 
   return (
     <div>
       <h1>{topic}</h1>
-      <p>Welcome, {myName}!</p>
-      <p>This is a React component. You can write JavaScript and HTML-like syntax together here.</p>
-      <p>Let's do some math: 2 + 2 = {2 + 2}</p>
+      <p>Promises traditionally use callbacks to resolve our asynchronous code.</p>
+      <p>We use the .then() callback to resolve successfully resolved promises,</p>
+      <p>while we use the .catch() callback to resolve promises that respond with an error.</p>
+      <p>But that can make our code harder to read and maintain.</p>
+      <p>Async/Await syntax provides a cleaner and more readable way to work with promises.</p>
+      <p>With Async/Await, we can write asynchronous code that looks and behaves like synchronous code.</p>
+      <p>This makes it easier to understand and maintain our code.</p>
+      <h2>GitHub Avatar of Sumit Kumar:</h2>
+      <img src={avatar} alt="Sumit Kumar"/>
+      <p>This avatar is fetched using Async/Await syntax.</p>
+      <p>I fetched my avatar from the GitHub API using the fetch function with Async/Await syntax.</p>
     </div>
   );
 }
